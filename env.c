b124a46c9ef9e9680c9bd9a8a926e792f2ffd029
fmgwfwt <132745147+fmgwfwt@users.noreply.github.com>
2023-05-29 12:43:34 +0300

    Update and rename env.cpp to env.c

---
+++ env.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+  char *value;
+  value = getenv("HOME");
+  printf("HOME= %s\n", value);
+  value = getenv("USER");
+  printf("USER= %s\n", value);
+  value = getenv("PASSWORD");
+  printf("PASSWORD= %s\n", value);
+  return 0;
+}
