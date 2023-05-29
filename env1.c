0085313479a2892c626105618fc0792eb09d60be
fmgwfwt <132745147+fmgwfwt@users.noreply.github.com>
2023-05-29 12:51:48 +0300

    Create env1.c

---
+++ env1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv, char** envp){
+    for(int i = 0;envp[i] != NULL;i++){
+        printf("%d: %s\n", i, envp[i]);
+    }
+
+    return 0;
+}
